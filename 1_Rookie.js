234f4c726df066f57613ca6c97cb299d08ecccea
Thomas David Kehoe <kehoe@casafuturatech.com>
2016-03-02 12:04:03 -0700

    8_Rooks_2ndLoop.js working on first loop only, ready to try second loop.

--- 1_Rookie.js
+++ 1_Rookie.js
@@ -1,15 +1,31 @@
-var columnRow = [[false, false],  [false, false],  [false, false],  [false, false], [false, false],  [false, false],  [false, false],  [false, false]];
-console.log(column);
+var columnRow = [ [false, false],  // sqr1A, sqr2A
+                  [false, false],  // sqr1B, sqr2B
+                  [false, false],
+                  [false, false],
+                  [false, false],
+                  [false, false],
+                  [false, false],
+                  [false, false]];
+console.log(columnRow);
 
 var placeRook = function(columnRow) {
   columnRow.forEach(function(square) {
+    // Column 1
     for (var i = 0; i < columnRow.length; i++) {
       var columnReduce = columnRow[0].reduce(function(previousValue, currentValue, currentIndex, array) {return previousValue + currentValue;});
       console.log(columnReduce);
       if (columnReduce === 0) {
-        columnRow[i][0] = true
-      }
-    }
+        columnRow[i][0] = true;
+      };
+    };
+    // Column 2
+    for (var i = 0; i < columnRow.length; i++) {
+      var columnReduce = columnRow[0][0].reduce(function(previousValue, currentValue, currentIndex, array) {return previousValue + currentValue;});
+      console.log(columnReduce);
+      if ( (columnReduce === 0) && (columnRow[0][i] + columnRow[0][i] === 0) ){ // sqr1A + sqr2A === 0
+        columnRow[0][i] = true;
+      };
+    };
   });
   return columnRow;
 };
