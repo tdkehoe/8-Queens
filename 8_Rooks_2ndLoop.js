26a12fad250650af974b676b110aff7fe03ae534
Thomas David Kehoe <kehoe@casafuturatech.com>
2016-03-02 13:07:31 -0700

    Added comments.

--- 8_Rooks_2ndLoop.js
+++ 8_Rooks_2ndLoop.js
@@ -9,12 +9,12 @@ var chessboard = [[false, false, false, false, false, false, false, false], // c
                  ];
 
 var placeRook = function(chessboard) {
-  chessboard.forEach(function(square) {
-    for (var j = 0; j < 8; j++) {
-      for (var i = 0; i < 8; i++) {
-        var columnReduce = chessboard[j].reduce(function(previousValue, currentValue, currentIndex, array) {return previousValue + currentValue;});
-        if ( (columnReduce === 0) && (chessboard[0][i] + chessboard[0][i] === 0) && (chessboard[1][i] + chessboard[1][i] === 0) && (chessboard[2][i] + chessboard[2][i] === 0) && (chessboard[3][i] + chessboard[3][i] === 0) && (chessboard[4][i] + chessboard[4][i] === 0) && (chessboard[5][i] + chessboard[5][i] === 0) && (chessboard[6][i] + chessboard[6][i] === 0) ) {
-          chessboard[j][i] = true;
+  chessboard.forEach(function(row) { // first loop: iterate through the eight rows of the chessboard
+    for (var j = 0; j < 8; j++) { // second loop: iterate through the eight squares of each row looking for rooks
+      for (var i = 0; i < 8; i++) { // third loop: iterate through the eight squares of each column looking for rooks
+        var columnReduce = chessboard[j].reduce(function(previousValue, currentValue, currentIndex, array) {return previousValue + currentValue;}); // adds the values one row of the chessboard, 0 = no rooks (safe to place a rook), 1 = a rook (not safe to place a rook)
+        if ( (columnReduce === 0) && (chessboard[0][i] === false) && (chessboard[1][i] === false) && (chessboard[2][i] === false) && (chessboard[3][i] === false) && (chessboard[4][i] === false) && (chessboard[5][i] === false) && (chessboard[6][i] === false)  && (chessboard[7][i] === false)) { // adds the values in one column of the chessboard
+          chessboard[j][i] = true; // place a rook in a square
         };
       };
     };
